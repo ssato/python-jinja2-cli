1bcbef85a9ddf7e48d0bdb3911c3ff574e363ad3
Satoru SATOH <satoru.satoh@gmail.com>
2012-08-30 13:37:36 +0900

    added test cases of glob pattern in context options

--- tests/test.sh
+++ tests/test.sh
@@ -2,4 +2,4 @@
 set -ex
 testsdir=${0%/*}
 cd ${testsdir}
-python ../jinja2_cui/cui.py -T . -C x.yaml -C yaml:y_yaml -o /tmp/test.out -D b.template
+python ../jinja2_cui/cui.py -T . -C x.yaml -C yaml:y_yaml -C yaml:conf.d/*.conf -o /tmp/test.out -D b.template
