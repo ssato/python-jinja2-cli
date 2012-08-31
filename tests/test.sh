f13061a4d63b2eedbbd84561e847be8de3e7d3a5
Satoru SATOH <satoru.satoh@gmail.com>
2012-08-31 14:14:12 +0900

    follow changes of command re-oraganizatin

--- tests/test.sh
+++ tests/test.sh
@@ -2,4 +2,4 @@
 set -ex
 testsdir=${0%/*}
 cd ${testsdir}
-python ../jinja2_cui/cui.py -T . -C x.yaml -C yaml:y_yaml -C yaml:conf.d/*.conf -o /tmp/test.out -D b.template
+PYTHONPATH=.. python ../src/jinja2-cui r -T . -C x.yaml -C yaml:y_yaml -C yaml:conf.d/*.conf -o /tmp/test.out -D b.template
