c14230732f0beef54e200e309626a6e75d29281d
Satoru SATOH <satoru.satoh@gmail.com>
2012-10-31 04:40:14 +0900

    fixed some pep8 (E302) errors

--- jinja2_cui/tests/dumpvars.py
+++ jinja2_cui/tests/dumpvars.py
@@ -2,6 +2,7 @@ import jinja2_cui.dumpvars as D
 import unittest
 from jinja2 import Environment, meta
 
+
 class Test_dumpvar(unittest.TestCase):
     def test_dumpvars(self):
         tmpl = '{{ a }}{{ b.b0 }}{{ c.c0.c1 + c.c0.c2 + c.c1}}'
@@ -12,6 +13,7 @@ class Test_dumpvar(unittest.TestCase):
         self.assertTrue(D.find_attrs(ast, 'c') == [['c', 'c0', 'c1'],
                                                    ['c', 'c0', 'c2'],
                                                    ['c', 'c1']])
+
 
 def suite():
     suite = unittest.TestSuite()
