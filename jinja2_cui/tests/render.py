c1abdbe6e1da45494e6d9c5ddd540804bfa63ee3
Satoru SATOH <satoru.satoh@gmail.com>
2012-10-31 21:03:18 +0900

    arrange placeholders for test target functions (actual tests not implemented yet)

--- jinja2_cui/tests/render.py
+++ jinja2_cui/tests/render.py
@@ -5,20 +5,50 @@ import jinja2_cui.render as R
 import unittest
 
 
-class Test_functions(unittest.TestCase):
+class Test_00_functions(unittest.TestCase):
 
-    def test_is_dict(self):
-        self.assertTrue(R.is_dict({}))
-        self.assertTrue(R.is_dict({"a": 1}))
-        self.assertFalse(R.is_dict([]))
-        self.assertFalse(R.is_dict(None))
+    def test_00_get_fileext(self):
+        self.assertEquals(R.get_fileext("a.json"), "json")
+        self.assertEquals(R.get_fileext("ajson"), "")
 
-    def test_is_iterable(self):
-        self.assertTrue(R.is_iterable([1, 2]))
-        self.assertTrue(R.is_iterable((1, 2)))
-        self.assertTrue(R.is_iterable((x for x in range(10))))
-        self.assertFalse(R.is_iterable({}))
-        self.assertFalse(R.is_iterable(None))
+    def test_10_find_loader(self):
+        pass
+
+    def test_20_load_context(self):
+        pass
+
+    def test_30_load_contexts(self):
+        pass
+
+    def test_40_mk_template_paths(self):
+        pass
+
+    def test_50_tmpl_env(self):
+        pass
+
+    def test_60_render_s(self):
+        pass
+
+    def test_70_render_impl(self):
+        pass
+
+    def test_80_render(self):
+        pass
+
+    def test_90_template_path(self):
+        pass
+
+    def test_100_get_ast(self):
+        pass
+
+    def test_110_parse_filespec(self):
+        pass
+
+    def test_120_parse_and_load_contexts(self):
+        pass
+
+    def test_130_parse_template_paths(self):
+        pass
 
 
 if __name__ == '__main__':
