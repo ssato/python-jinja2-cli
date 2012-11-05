3b7fc99d651d78a9ff1a02641f2833cfa921ed03
Satoru SATOH <satoru.satoh@gmail.com>
2012-11-05 18:09:33 +0900

    removed test cases for functions moved to jinja2_cui.contexts

--- jinja2_cui/tests/render.py
+++ jinja2_cui/tests/render.py
@@ -7,47 +7,28 @@ import unittest
 
 class Test_00_functions(unittest.TestCase):
 
-    def test_00_get_fileext(self):
-        self.assertEquals(R.get_fileext("a.json"), "json")
-        self.assertEquals(R.get_fileext("ajson"), "")
-
-    def test_10_find_loader(self):
+    def test_00_mk_template_paths(self):
         pass
 
-    def test_20_load_context(self):
+    def test_10_tmpl_env(self):
         pass
 
-    def test_30_load_contexts(self):
+    def test_20_render_s(self):
         pass
 
-    def test_40_mk_template_paths(self):
+    def test_30_render_impl(self):
         pass
 
-    def test_50_tmpl_env(self):
+    def test_40_render(self):
         pass
 
-    def test_60_render_s(self):
+    def test_50_template_path(self):
         pass
 
-    def test_70_render_impl(self):
+    def test_60_get_ast(self):
         pass
 
-    def test_80_render(self):
-        pass
-
-    def test_90_template_path(self):
-        pass
-
-    def test_100_get_ast(self):
-        pass
-
-    def test_110_parse_filespec(self):
-        pass
-
-    def test_120_parse_and_load_contexts(self):
-        pass
-
-    def test_130_parse_template_paths(self):
+    def test_70_parse_template_paths(self):
         pass
 
 
