c99c218fc0a668026d0797d426980a6de2069799
Satoru SATOH <satoru.satoh@gmail.com>
2012-11-05 18:08:15 +0900

    fixed some errors and made it works

--- jinja2_cui/tests/contexts.py
+++ jinja2_cui/tests/contexts.py
@@ -8,13 +8,13 @@ import unittest
 class Test_00_functions(unittest.TestCase):
 
     def test_00_get_fileext(self):
-        self.assertEquals(R.get_fileext("a.json"), "json")
-        self.assertEquals(R.get_fileext("ajson"), "")
+        self.assertEquals(C.get_fileext("a.json"), "json")
+        self.assertEquals(C.get_fileext("ajson"), "")
 
     def test_10_find_loader(self):
         mockloaders = dict(json=0, yaml=1)
-        self.assertEquals(C.find_loader("a.json"), loaders=mockloaders),  0)
-        self.assertEquals(C.find_loader("a.yaml"), loaders=mockloaders),  1)
+        self.assertEquals(C.find_loader("a.json", loaders=mockloaders),  0)
+        self.assertEquals(C.find_loader("a.yaml", loaders=mockloaders),  1)
 
     def test_20_load_context(self):
         pass
