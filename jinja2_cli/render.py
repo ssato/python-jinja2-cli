3986d39c8fab4667d1a5b70e60139a47f0468202
Satoru SATOH <satoru.satoh@gmail.com>
2013-05-03 21:27:16 +0900

    fixes for PEP8 errors: E124, E128 and E502

--- jinja2_cli/render.py
+++ jinja2_cli/render.py
@@ -241,25 +241,24 @@ def option_parser(argv=sys.argv):
     p.set_defaults(**defaults)
 
     p.add_option("-T", "--template-paths",
-        help="Colon ':' separated template search paths. " + \
-            "Please note that dir in which given template exists " + \
-            "is always included in the search paths (at the end of " + \
-            "the path list) regardless of this option. " + \
-            "[., dir in which given template file exists]")
+                 help="Colon ':' separated template search paths. "
+                 "Please note that dir in which given template exists "
+                 "is always included in the search paths (at the end of "
+                 "the path list) regardless of this option. "
+                 "[., dir in which given template file exists]")
     p.add_option("-C", "--contexts", action="append",
-        help="Specify file path and optionally its filetype, to provides "
-            " context data to instantiate templates. "
-            " The option argument's format is "
-            " [type:]<file_name_or_path_or_glob_pattern>"
-            " ex. -C json:common.json -C ./specific.yaml -C yaml:test.dat, "
-            "     -C yaml:/etc/foo.d/*.conf"
-    )
+                 help="Specify file path and optionally its filetype, to "
+                 "provides context data to instantiate templates. "
+                 " The option argument's format is "
+                 " [type:]<file_name_or_path_or_glob_pattern>"
+                 " ex. -C json:common.json -C ./specific.yaml -C "
+                 "yaml:test.dat, -C yaml:/etc/foo.d/*.conf")
     p.add_option("-o", "--output", help="Output filename [stdout]")
     p.add_option("-E", "--encoding", help="I/O encoding [%default]")
     p.add_option("-D", "--debug", action="store_true", help="Debug mode")
     p.add_option("-W", "--werror", action="store_true",
-        help="Exit on warnings if True such like -Werror optoin for gcc"
-    )
+                 help="Exit on warnings if True such like -Werror optoin "
+                 "for gcc")
 
     return p
 
